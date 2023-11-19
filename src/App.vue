91cb92a284a6cf091e7760e35f4cfdfb9f3e5ff6
HugoAlejandro2002 <apazahuaychohugoalejandro@gmail.com>
2023-11-19 16:52:56 -0400

    added tailwind to the proyect

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,18 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
+  <div class="flex justify-center items-center">
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="/vite.svg" class="h-24 transition duration-300 ease-in-out hover:drop-shadow-lg" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="./assets/vue.svg" class="h-24 transition duration-300 ease-in-out hover:drop-shadow-lg" alt="Vue logo" />
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,21 +9,22 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1 class="text-2xl font-bold">{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
+  <div class="card bg-gray-100 p-4 rounded">
+    <button type="button" @click="count++" class="bg-blue-500 text-white font-bold py-2 px-4 rounded">
+      count is {{ count }}
+    </button>
+    <p class="text-gray-600">
       Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+      <code class="bg-gray-200 rounded p-1">components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+    starter
   </p>
   <p>
     Install
